16cf6cd54cabdb5f50ccc86e8c219b727eeb5c22
Florian Abélard <f.abelard@gmail.com>
2020-11-02 10:17:57 +0100

    chore(makefile): launch phpunit with a filter argument

--- makefiles/phpunit.mk
+++ makefiles/phpunit.mk
@@ -4,10 +4,19 @@
 
 PHPUNIT_DOCKER_CMD = docker-compose run -T --user ${USER_ID}:${GROUP_ID} php vendor/bin/simple-phpunit ${1}
 
+# Cli arguments
+ifneq (,$(filter phpunit-filter, $(firstword $(MAKECMDGOALS))))
+    PHPUNIT_FILTER_ARG := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+    $(eval $(PHPUNIT_FILTER_ARG):;@:)
+endif
+
 #------------------------------------------------------------------------------
 
 phpunit: db-init-for-test ##@phpunit launch PHPUnit tests
 	$(call PHPUNIT_DOCKER_CMD, --verbose)
+
+phpunit-filter: db-init-for-test ##@phpunit launch PHPUnit command with the filter passed in argument
+	$(call PHPUNIT_DOCKER_CMD, --filter=$(PHPUNIT_FILTER_ARG) --verbose)
 
 phpunit-install-env: ##@phpunit install PHPUnit in environment
 	docker-compose -f ${DOCKER_COMPOSE_BUILDER_FILE} run -T --rm --user ${USER_ID}:${GROUP_ID} composer ./vendor/bin/simple-phpunit install
